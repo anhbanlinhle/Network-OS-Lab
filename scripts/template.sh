7eec7590061bcf7d7b18361e3ca50e3d8f7c0ff5
anhbanlinhle <vietlinhleviet@gmail.com>
2023-11-14 22:47:05 +0700

    auto generate commands

--- scripts/template.sh
+++ scripts/template.sh
@@ -30,6 +30,21 @@ exec 3<> lesson_$lesson/$os/task_$task.md
   do
     echo "- " >&3
   done
+
+  echo '\n<a name="solutions"/>\n' >&3
+  echo '# Solutions \n' >&3
+  echo '<a name="commands"/>\n' >&3
+  echo '## Commands\n' >&3
+  
+  for i in `seq 1 $cmd`
+  do
+    echo "<a name=\"command-$i\"/>\n" >&3
+    echo "### Command $i [↑](#bookmarks) [↓](#result-$i)\n" >&3
+    echo '```sh' >&3
+    echo '' >&3
+    echo '```\n' >&3  
+  done
+  
 exec 3>&-
 
 echo "File path: $(ls lesson_$lesson/$os/task_$task.md)";
