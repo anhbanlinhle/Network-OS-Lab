b0173b276695ac55be4f7b349c1b0021d189c144
anhbanlinhle <vietlinhleviet@gmail.com>
2023-11-14 22:30:39 +0700

    auto generate bookmark

---
+++ scripts/template.sh
@@ -0,0 +1,36 @@
+echo "Lesson number (XX): "; read lesson
+echo "OS name (macos/linux): "; read os
+echo "Task number (YY): "; read task
+echo "Expected commands (Z): "; read cmd
+
+mkdir lesson_$lesson lesson_$lesson/linux lesson_$lesson/macos
+
+exec 3<> lesson_$lesson/$os/task_$task.md
+
+  echo '<a name="bookmarks"/>\n' >&3
+  echo '# Bookmarks\n' >&3
+  echo '- [Requirements](#requirements)\n' >&3
+  echo '- [Solutions](#solutions)\n' >&3
+  echo '\t- [Commands](#commands)' >&3
+  
+  for i in `seq 1 $cmd`
+  do
+    echo "\t\t- [Command $i](#command-$i)" >&3
+  done
+
+  echo '\n\t- [Results](#results)' >&3
+  for i in `seq 1 $cmd`
+  do
+    echo "\t\t- [Result of Command $i](#result-$i)" >&3
+  done
+  
+
+echo "File path: $(ls lesson_$lesson/$os/task_$task.md)";
+
+# <a name="bookmarks"/>
+
+# Bookmarks
+
+#- [Requirements](#requirements)
+
+#- [Solutions](#solutions)
